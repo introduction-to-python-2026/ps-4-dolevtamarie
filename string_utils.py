6f3bedf1161d928f4c074fc637094f43a2c7b845
dolevtamarie <dolevtamarie@gmail.com>
2025-11-13 16:47:24 +0200

    Refactor string splitting logic in string_utils.py
    Refactor string splitting functions to use index-based loops instead of enumerate.

--- string_utils.py
+++ string_utils.py
@@ -3,19 +3,17 @@ def split_before_each_uppercase(formula):
         return []
     parts = []
     start = 0
-    for i, char in enumerate(formula[1:], start=1):
-        if char.isupper():
+    for i in range(1, len(formula)):
+        if formula[i].isupper():
             parts.append(formula[start:i])
             start = i
     parts.append(formula[start:])
     return parts
 
 def split_at_digit(formula):
-    for i, char in enumerate(formula):
-        if char.isdigit():
-            return (formula[:i], int(formula[i:]))
+    for i in range(len(formula)):
+        if formula[i].isdigit():
+            prefix = formula[:i]
+            number = int(formula[i:])
+            return (prefix, number)
     return (formula, 1)
-
-def split_formula(formula):
-    return [split_at_digit(part) for part in split_before_each_uppercase(formula) if part]
-    
